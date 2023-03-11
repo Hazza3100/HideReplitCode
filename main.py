8d231e40694d75bf869bf9067eb83e99ec5d68ca
Hazza <86918289+Hazza3100@users.noreply.github.com>
2023-03-11 15:56:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 # you can use https://www.base64encode.org/ to encode your code
 
 x = input('encoded code: ')
-print(f'Your env: {x}\nYour public code: import base64; exec(base64.b64decode("{x}"))')
+print(f'Your env: {x}\nYour public code: import base64; x = "{x}"; exec(base64.b64decode(f"{x}"))')
